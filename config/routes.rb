f51b2275d3f93408648a06b2377860bf569db356
Cory Walker <cory.walker.dev@gmail.com>
2023-01-20 10:03:05 -0800

    Create Event and EventController

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  get 'event/new'
+  get 'event/create'
+  get 'event/show'
+  get 'event/index'
+  get 'event/edit'
+  get 'event/destroy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+   root "events#index"
 end
